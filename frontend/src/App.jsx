7badd20a217f7688d7ae31e6973151cf0580b04a
YASIKA.IT21 <yasika.it21@bitsathy.ac.in>
2025-04-13 00:35:21 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Cartpage from "./pages/Cartpage.jsx";
+import Register from "./pages/Register.jsx";
+import Loginpage from "./pages/Loginpage.jsx";
+import ProductDetailsPage from "./pages/ProductDetailsPage.jsx";
+import ProductListPage from "./pages/ProductListPage.jsx";
+import UserOrderPage from "./pages/user/UsersOrderPage.jsx";
+import Usercartdetailspage from "./pages/user/Usercartdetailspage.jsx";
+import Userprofilepage from "./pages/user/Userprofilepage.jsx";
+import Userorderdetailspage from "./pages/user/Userorderdetailspage.jsx";
+import ProtectedRouterComponent from "./components/ProtectedRouterComponent.jsx";
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage/>}> </Route>
+      <Route path="/cart" element={<Cartpage/>}> </Route>
+      <Route path="/register" element={<Register/>}> </Route>
+      <Route path="/product-list" element={<ProductListPage/>}> </Route>
+      <Route path="/product-details/:id" element={<ProductDetailsPage/>}> </Route>
+      <Route path="/login" element={<Loginpage/>}> </Route>
+      <Route path="*" element="Page not found 404"> </Route>
+      <Route element={<ProtectedRouterComponent/>}> 
+           <Route path="/user-cart-details" element={<Usercartdetailspage/>}> </Route>
+           <Route path="/user-order-details" element={<Userorderdetailspage/>}> </Route>
+           <Route path="/user" element={<Userprofilepage/>}> </Route>
+           <Route path="/user-order-page" element={<UserOrderPage/>}> </Route>
+      </Route>
+      
+    
+    </Routes>
+    </BrowserRouter>
+   
+
+  );
 }
 
 export default App
